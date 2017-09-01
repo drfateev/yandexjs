c2c5b397173b73c667b3b50850c61c7796a7da40
drfateev <drfateev@gmail.com>
2017-09-01 22:41:17 +0300

    2nd commit

--- index.js
+++ index.js
@@ -9,25 +9,29 @@ $('document').ready(function() {
         $('#email,#phone,#fio,#resultContainer').removeClass('error');
         $('#resultContainer').html('');
         event.preventDefault();
-        //validation
-        if($('#fio').val().trim().split(/\s+/).length === 3){
-            if(mailtest.test($('#email').val())) {
-                digit=$('#phone').val();
-                totalDigit=parseInt(digit[1])+parseInt(digit[3])+parseInt(digit[4])+parseInt(digit[5])+parseInt(digit[7])+parseInt(digit[8])+parseInt(digit[9])+parseInt(digit[11])+parseInt(digit[12])+parseInt(digit[14])+parseInt(digit[15]);
-                if(phonetest.test($('#phone').val())&&totalDigit<31) {
-                    $('#submitButton').attr('disabled',true);
-                    $('#resultContainer').addClass('success').html('<p>Success</p>');
+        if (!$('#fio').val()||!$('#email').val()||!$('#phone').val()) {
+            $('#resultContainer').html('<p>Progress: Fill all fields</p>');
+        } else {
+            //validation
+            if($('#fio').val().trim().split(/\s+/).length === 3){
+                if(mailtest.test($('#email').val())) {
+                    digit=$('#phone').val();
+                    totalDigit=parseInt(digit[1])+parseInt(digit[3])+parseInt(digit[4])+parseInt(digit[5])+parseInt(digit[7])+parseInt(digit[8])+parseInt(digit[9])+parseInt(digit[11])+parseInt(digit[12])+parseInt(digit[14])+parseInt(digit[15]);
+                    if(phonetest.test($('#phone').val())&&totalDigit<31) {
+                        $('#submitButton').attr('disabled',true);
+                        $('#resultContainer').addClass('success').html('<p>Success</p>');
+                    } else {
+                        $('#phone').addClass('error');
+                        $('#resultContainer').addClass('error').html('<p>Reason: Wrong Phone Number</p>');
+                    }
                 } else {
-                    $('#phone').addClass('error');
-                    $('#resultContainer').addClass('error').html('<p>Reason: Wrong Phone Number</p>');
+                    $('#email').addClass('error');
+                    $('#resultContainer').addClass('error').html('<p>Reason: Wrong Email</p>');
                 }
             } else {
-                $('#email').addClass('error');
-                $('#resultContainer').addClass('error').html('<p>Reason: Wrong Email</p>');
+                $('#fio').addClass('error');
+                $('#resultContainer').addClass('error').html('<p>Reason: Wrong Name</p>');
             }
-        } else {
-            $('#fio').addClass('error');
-            $('#resultContainer').addClass('error').html('<p>Reason: Wrong Name</p>');
         }
 
     });
